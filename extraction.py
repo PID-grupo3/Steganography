090096e9090c7a57bf55bd4e77187842b42eac3f
juamunleo <2ejmunoz@gmail.com>
2021-05-26 14:22:05 +0200

    Updated extraction code

--- extraction.py
+++ extraction.py
@@ -2,15 +2,12 @@ import cv2
 from extractionAuxiliar import decode_image
 import matplotlib.pyplot as plt
 def extraction(imagen):
-    mensage,heatMap,imgBlock,EncriptedImg= decode_image(imagen)
+    mensaje,heatMap,imgBlock,EncriptedImg = decode_image(imagen)
+    return mensaje,heatMap,imgBlock,EncriptedImg
 
-    print(mensage)
-    plt.plot(heatMap)
-    print(imgBlock)
-    print(EncriptedImg)
-    return mensage
+    
 
 photo="images/saved.png"
 dct_img = cv2.imread(photo, -1)
-
-extraction(dct_img)
+mensaje,heatMap,imgBlock,EncriptedImg = extraction(dct_img)
+plt.show()
